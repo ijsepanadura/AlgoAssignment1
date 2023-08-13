88683202515935e929a21938a56121f29525865b
Ishan Lahiru <ishanlahiru2012@gmail.com>
2023-08-13 22:43:18 +0530

    comment adding

--- Algo5.java
+++ Algo5.java
@@ -7,18 +7,18 @@ public class Algo5 {
         do{
             String reverse="";
             System.out.print("Enter a text : ");
-            String name = scanner.nextLine().strip();
+            String name = scanner.nextLine().strip(); // getting input and space remove
 
-            if(name.length()==0){
+            if(name.length()==0){           // checking weither the input is empty
                 System.out.println("Text cannot be Empty..");
                 continue;
             }
             for (int i = 0; i < name.length(); i++) {
-                if(name.charAt(i)<0 || name.charAt(i)>127){
+                if(name.charAt(i)<0 || name.charAt(i)>127){ //checking weither the input is ASCII
                     System.out.println("you can only use ASCII input ! try again..");
                     continue loop;
                 }
-                reverse = Character.toString(name.charAt(i)) + reverse;
+                reverse = Character.toString(name.charAt(i)) + reverse;  // input inverting 
             }
             System.out.println("reversed input is : " + reverse);
         }while(true);   
--- Algo6.java
+++ Algo6.java
@@ -7,21 +7,24 @@ public class Algo6 {
         do{
             String name;
             String[] word;
+            String[] reverse;
             System.out.print("Enter a sentence: ");
-            name = scanner.nextLine().strip();
-            if(name.length()==0){
+            name = scanner.nextLine().strip();          // getting user input
+            if(name.length()==0){                       // checking weither the input is empty
                 System.out.println("sentence cannot be Empty..");
                 continue;
             }
             for (int i = 0; i < name.length(); i++) {
-                if(name.charAt(i)<0 || name.charAt(i)>127 ){
+                if(name.charAt(i)<0 || name.charAt(i)>127 ){  // checking weither the input is ASSCII
                     System.out.println("You can only use ASCII charactors ! try again..");
                     continue loop;
                 }  
             }
-            word = name.split(" ");
+            word = name.split(" ");         // split word by space
+            reverse = new String[word.length];
             for(int i=0; i< word.length; i++){
-                System.out.print(word[word.length-1-i]+" ");
+                reverse[i] = word[word.length-1-i];     // inverting array
+                System.out.print(reverse[i]+" ");       // display reverse sentence
             }
             System.out.println();
         }while(true);
